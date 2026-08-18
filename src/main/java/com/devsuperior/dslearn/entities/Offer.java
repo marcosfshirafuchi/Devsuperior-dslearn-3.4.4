bf2bf1fbeb84f25057e1e88145832d1cd82538a2
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-08-18 20:08:07 -0300

    Fix: offer timestamp without time zone

--- src/main/java/com/devsuperior/dslearn/entities/Offer.java
+++ src/main/java/com/devsuperior/dslearn/entities/Offer.java
@@ -17,7 +17,9 @@ public class Offer implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String edition;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant startMoment;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant endMoment;
 
     //Um curso tem muitas ofertas ou muitas ofertas tem um curso
